333a89f5c79e5cde497deac8afea9a82b01e1b4c
takayayamazoe <mt.tky@icloud.com>
2017-06-16 16:22:02 +0900

    html追加

---
+++ ref/test_database/index.js
@@ -0,0 +1,57 @@
+'use strict'
+
+
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+var sqlite3 = require('sqlite3').verbose();   
+var db = new sqlite3.Database('db.sqlite3');
+
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', function(req, res) {
+	//let players = Player.objects('Player');
+	db.all("SELECT * FROM players",function(err, rows){
+      if (!err) {
+        res.render('index.ejs', {
+        	players : rows
+        }); 
+      }   
+    }); 
+	//res.render('index.ejs', {players: players});
+});
+
+
+app.get('/write', function(req, res) {
+	res.sendFile(__dirname + "/write.html");
+});
+
+app.post('/insert', function(req, res) {
+	console.log(req.body);
+
+	let name = req.body['name'],
+		lat = req.body['pos'].lat,
+		long = req.body['pos'].long;
+
+	console.log(name,lat,long);
+	
+	console.log("I'm here");
+	db.run("INSERT INTO players VALUES (?,?,?)", name, lat, long);
+
+	res.send(true);
+});
+
+app.use(function (req, res, next) {
+	res.status(404).send("File not found");
+});
+
+
+app.listen(3000, function() {
+	console.log("Go!");
+});
+
