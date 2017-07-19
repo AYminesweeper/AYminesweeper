3ece6633131e21e2723bcf2d3cfb32fd9ecacdaa
tomkenta <tomkenta2014@gmail.com>
2017-07-19 12:08:46 +0900

    add buttons effect sounds

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,64 +1,77 @@
-jQuery(document).ready(function($){
-	//cache some jQuery objects
-	var modalTrigger = $('.cd-modal-trigger'),
-		transitionLayer = $('.cd-transition-layer'),
-		transitionBackground = transitionLayer.children(),
-		modalWindow = $('.cd-modal');
+jQuery(document).ready(function($) {
+    //cache some jQuery objects
+    var modalTrigger = $('.cd-modal-trigger'),
+        buttonTrigger = $('.cd-btn'),
+        transitionLayer = $('.cd-transition-layer'),
+        transitionBackground = transitionLayer.children(),
+        modalWindow = $('.cd-modal');
 
-	var frameProportion = 1.78, //png frame aspect ratio
-		frames = transitionLayer.data('frame'), //number of png frames
-		resize = false;
+    var frameProportion = 1.78, //png frame aspect ratio
+        frames = transitionLayer.data('frame'), //number of png frames
+        resize = false;
 
-	//set transitionBackground dimentions
-	setLayerDimensions();
-	$(window).on('resize', function(){
-		if( !resize ) {
-			resize = true;
-			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
-		}
-	});
+    //set transitionBackground dimentions
+    setLayerDimensions();
+    $(window).on('resize', function() {
+        if (!resize) {
+            resize = true;
+            (!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300): window.requestAnimationFrame(setLayerDimensions);
+        }
+    });
 
-	//open modal window
-	modalTrigger.on('click', function(event){	
-		event.preventDefault();
-		var modalId = $(event.target).attr('href');
-		transitionLayer.addClass('visible opening');
-		var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 800;
-		setTimeout(function(){
-			modalWindow.filter(modalId).addClass('visible');
-			transitionLayer.removeClass('opening');
-		}, delay);
-	});
+    //open modal window
+    modalTrigger.on('click', function(event) {
+        event.preventDefault();
+        var modalId = $(event.target).attr('href');
+        transitionLayer.addClass('visible opening');
+        var delay = ($('.no-cssanimations').length > 0) ? 0 : 800;
+        setTimeout(function() {
+            modalWindow.filter(modalId).addClass('visible');
+            transitionLayer.removeClass('opening');
+        }, delay);
+    });
 
-	//close modal window
-	modalWindow.on('click', '.modal-close', function(event){
-		event.preventDefault();
-		transitionLayer.addClass('closing');
-		modalWindow.removeClass('visible');
-		transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
-			transitionLayer.removeClass('closing opening visible');
-			transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
-		});
-	});
+    buttonTrigger.on('click', function(event) {
+            $('#sound-file').get(0).play();
+            var href = $(event.target).attr('ref');
+            var delay = 500;
+            console.log(href);
+            if (href) {
+                setTimeout(function() {
+                    location.href = href;
+                }, delay);
+            }
+        });
 
-	function setLayerDimensions() {
-		var windowWidth = $(window).width(),
-			windowHeight = $(window).height(),
-			layerHeight, layerWidth;
+//close modal window
+modalWindow.on('click', '.modal-close', function(event) {
+    event.preventDefault();
+    transitionLayer.addClass('closing');
+    modalWindow.removeClass('visible');
+    transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+        transitionLayer.removeClass('closing opening visible');
+        transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+    });
+});
 
-		if( windowWidth/windowHeight > frameProportion ) {
-			layerWidth = windowWidth;
-			layerHeight = layerWidth/frameProportion;
-		} else {
-			layerHeight = windowHeight*1.2;
-			layerWidth = layerHeight*frameProportion;
-		}
+function setLayerDimensions() {
+    var windowWidth = $(window).width(),
+        windowHeight = $(window).height(),
+        layerHeight, layerWidth;
 
-		transitionBackground.css({
-			'width': layerWidth*frames+'px',
-			'height': layerHeight+'px',
-		});
+    if (windowWidth / windowHeight > frameProportion) {
+        layerWidth = windowWidth;
+        layerHeight = layerWidth / frameProportion;
+    } else {
+        layerHeight = windowHeight * 1.2;
+        layerWidth = layerHeight * frameProportion;
+    }
 
-		resize = false;
-	}
+    transitionBackground.css({
+        'width': layerWidth * frames + 'px',
+        'height': layerHeight + 'px',
+    });
+
+    resize = false;
+}
 });
